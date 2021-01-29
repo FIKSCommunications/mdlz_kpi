45506d8f7b82043e8f6caf20b700c6455118d692
YujiTanioka <fikstanioka0401@gmail.com>
2021-01-29 14:51:35 +0900

    結果画面　編集

---
+++ components/summaryGraph.vue
@@ -0,0 +1,142 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{title}}
+    </v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col cols="6">
+          <v-row>
+            <v-col cols="12">
+              <v-sheet
+                color="white"
+                elevation="2"
+              >
+                <v-simple-table dense>
+                  <template v-slot:default>
+                    <thead>
+                      <tr>
+                        <th v-for="(col, index) in cols" :key="index" class="text-left">
+                          {{col}}
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{{ all }}</td>
+                        <td>{{ rate }}%</td>
+                        <td>{{ num }}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-sheet>
+            </v-col>
+          </v-row>
+          <v-row justify="end" align="center">
+            <v-col cols="4">
+              <v-btn 
+              color="primary" 
+              rounded 
+              small  
+              text
+              class="text-decoration-underline"
+              @click="viewDetail"
+              >
+              <v-icon x-small>{{viewFlg==false?'mdi-plus':'mdi-minus'}}</v-icon>
+              {{viewFlg==false?'詳細':'詳細非表示'}}</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col class=mt-3>
+          <v-progress-linear
+            :value="rate"
+            :color="rate >= 100 ? 'green' : 'warning' "
+            height="20"
+          >
+            <strong>{{rate}}%</strong>
+          </v-progress-linear>
+        </v-col>
+      </v-row>
+      <v-row v-show="viewFlg">
+        <v-col cols="12">
+          <v-sheet
+            color="white"
+            elevation="2"
+          >
+            <v-simple-table 
+            dense
+            fixed-header
+            >
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th>企業</th>
+                    <th v-for="(col, index) in cols" :key="index" class="text-left">
+                      {{col}}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(data, index) in detalData" :key="index">
+                    <td>{{data.chq}}</td>
+                    <td>{{ data.all }}</td>
+                    <td>{{ data.rate }}%</td>
+                    <td>{{ data.num }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      viewFlg : false,
+      detalData : [
+        {
+          chq:'イオン',
+          all:100,
+          num:20,
+          rate:20
+        },
+        {
+          chq:'ダイエー',
+          all:10,
+          num:10,
+          rate:100
+        },
+        {
+          chq:'イトーヨーカドー',
+          all:100,
+          num:30,
+          rate:30
+        },
+      ],
+    }
+  },
+  methods:{
+    viewDetail:function(){
+      if (this.viewFlg == true) {
+        this.viewFlg = false;
+      } else {
+        this.viewFlg = true;
+      }
+      console.log(this.viewFlg)
+    },
+  },
+  props:[
+    'title',
+    'cols',
+    'all',
+    'rate',
+    'num',
+  ]
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -41,292 +41,74 @@
     </v-row>
     <v-row justify="center" align="center" v-show="viewFlgSummary">
       <v-col cols="8">
-        <v-card>
-          <v-card-title>
-            ①　HZ占有率
-          </v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">
-                <v-simple-table dense>
-                  <template v-slot:default>
-                    <thead>
-                      <tr>
-                        <th class="text-left">
-                          レジ台数
-                        </th>
-                        <th class="text-left">
-                          所有率
-                        </th>
-                        <th class="text-left">
-                          カバレッジ
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{{ hzGetData.all }}</td>
-                        <td>{{ hzGetData.rate }}</td>
-                        <td>{{ hzGetData.num }}</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-col>
-              <v-col class=mt-3>
-                <v-progress-linear
-                  :value="hzGetData.rate"
-                  :color="hzGetData.rate == 100 ? 'green' : 'warning' "
-                  height="20"
-                >
-                  <strong>{{hzGetData.rate}}%</strong>
-                </v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-
-    <v-row justify="center" align="center" v-show="viewFlgSummary">
-      <v-col cols="8">
-        <v-card>
-          <v-card-title>
-            ②　HZ総拠点数
-          </v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">
-                <v-simple-table dense>
-                  <template v-slot:default>
-                    <thead>
-                      <tr>
-                        <th class="text-left">
-                          レジ台数
-                        </th>
-                        <th class="text-left">
-                          所有率
-                        </th>
-                        <th class="text-left">
-                          カバレッジ
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{{ hzAllData.all }}</td>
-                        <td>{{ hzAllData.rate }}</td>
-                        <td>{{ hzAllData.num }}</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-col>
-              <v-col class=mt-3>
-                <v-progress-linear
-                  :value="hzAllData.rate"
-                  :color="hzAllData.rate == 100 ? 'green' : 'warning' "
-                  height="20"
-                >
-                  <strong>{{hzAllData.rate}}%</strong>
-                </v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-
-    <v-row justify="center" align="center" v-show="viewFlgSummary">
-      <v-col cols="8">
-        <v-card>
-          <v-card-title>
-            ③　SD総拠点数
-          </v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">
-                <v-simple-table dense>
-                  <template v-slot:default>
-                    <thead>
-                      <tr>
-                        <th class="text-left">
-                          レジ台数
-                        </th>
-                        <th class="text-left">
-                          所有率
-                        </th>
-                        <th class="text-left">
-                          カバレッジ
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{{ sdAllData.all }}</td>
-                        <td>{{ sdAllData.rate }}</td>
-                        <td>{{ sdAllData.num }}</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-col>
-              <v-col class=mt-3>
-                <v-progress-linear
-                  :value="sdAllData.rate"
-                  :color="sdAllData.rate == 100 ? 'green' : 'warning' "
-                  height="20"
-                >
-                  <strong>{{sdAllData.rate}}%</strong>
-                </v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+        <summaryGraph
+        :title="hzGetData.title"
+        :cols="hzGetData.cols"
+        :all="hzGetData.all"
+        :rate="hzGetData.rate"
+        :num="hzGetData.num"
+        >
+        </summaryGraph>
       </v-col>
     </v-row>
     <v-row justify="center" align="center" v-show="viewFlgSummary">
       <v-col cols="8">
-        <v-card>
-          <v-card-title>
-            ④　DP設置台数
-          </v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">
-                <v-simple-table dense>
-                  <template v-slot:default>
-                    <thead>
-                      <tr>
-                        <th class="text-left">
-                          レジ台数
-                        </th>
-                        <th class="text-left">
-                          所有率
-                        </th>
-                        <th class="text-left">
-                          カバレッジ
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{{ dpData.all }}</td>
-                        <td>{{ dpData.rate }}</td>
-                        <td>{{ dpData.num }}</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-col>
-              <v-col class=mt-3>
-                <v-progress-linear
-                  :value="dpData.rate"
-                  :color="dpData.rate == 100 ? 'green' : 'warning' "
-                  height="20"
-                >
-                  <strong>{{dpData.rate}}%</strong>
-                </v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  <v-row justify="center" align="center" v-show="viewFlgSummary">
-        <v-col cols="8">
-          <v-card>
-            <v-card-title>
-              ⑥　インプロ金額
-            </v-card-title>
-            <v-card-text>
-              <v-row>
-                <v-col cols="6">
-                  <v-simple-table dense>
-                    <template v-slot:default>
-                      <thead>
-                        <tr>
-                          <th class="text-left">
-                            レジ台数
-                          </th>
-                          <th class="text-left">
-                            所有率
-                          </th>
-                          <th class="text-left">
-                            カバレッジ
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>{{ displayData.all }}</td>
-                          <td>{{ displayData.rate }}</td>
-                          <td>{{ displayData.num }}</td>
-                        </tr>
-                      </tbody>
-                    </template>
-                  </v-simple-table>
-                </v-col>
-                <v-col class=mt-3>
-                  <v-progress-linear
-                    :value="displayData.rate"
-                    :color="displayData.rate == 100 ? 'green' : 'warning' "
-                    height="20"
-                  >
-                  <strong>{{displayData.rate}}%</strong>
-                </v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+        <summaryGraph
+        :title="hzAllData.title"
+        :cols="hzAllData.cols"
+        :all="hzAllData.all"
+        :rate="hzAllData.rate"
+        :num="hzAllData.num"
+        >
+        </summaryGraph>
       </v-col>
     </v-row>
     <v-row justify="center" align="center" v-show="viewFlgSummary">
       <v-col cols="8">
-        <v-card>
-          <v-card-title>
-            ⑤　大陳列
-          </v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">
-                <v-simple-table dense>
-                  <template v-slot:default>
-                    <thead>
-                      <tr>
-                        <th class="text-left">
-                          レジ台数
-                        </th>
-                        <th class="text-left">
-                          所有率
-                        </th>
-                        <th class="text-left">
-                          カバレッジ
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{{ inproData.all }}</td>
-                        <td>{{ inproData.rate }}</td>
-                        <td>{{ inproData.num }}</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-col>
-              <v-col class=mt-3>
-                <v-progress-linear
-                  :value="inproData.rate"
-                  :color="inproData.rate == 100 ? 'green' : 'warning' "
-                  height="20"
-                >
-                  <strong>{{inproData.rate}}%</strong>
-                </v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+        <summaryGraph
+        :title="sdAllData.title"
+        :cols="sdAllData.cols"
+        :all="sdAllData.all"
+        :rate="sdAllData.rate"
+        :num="sdAllData.num"
+        >
+        </summaryGraph>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" v-show="viewFlgSummary">
+      <v-col cols="8">
+        <summaryGraph
+        :title="dpData.title"
+        :cols="dpData.cols"
+        :all="dpData.all"
+        :rate="dpData.rate"
+        :num="dpData.num"
+        >
+        </summaryGraph>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" v-show="viewFlgSummary">
+      <v-col cols="8">
+        <summaryGraph
+        :title="displayData.title"
+        :cols="displayData.cols"
+        :all="displayData.all"
+        :rate="displayData.rate"
+        :num="displayData.num"
+        >
+        </summaryGraph>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" v-show="viewFlgSummary">
+      <v-col cols="8">
+        <summaryGraph
+        :title="inproData.title"
+        :cols="inproData.cols"
+        :all="inproData.all"
+        :rate="inproData.rate"
+        :num="inproData.num"
+        >
+        </summaryGraph>
       </v-col>
     </v-row>
   </div>
@@ -334,39 +116,56 @@
 
 <script>
 import moment from "moment";
+import summaryGraph from "../components/summaryGraph";
 export default {
-
+  components:{
+    summaryGraph,
+  },
   data () {
     return {
       hzGetData:{
+        title:'①　HZ占有率',
+        cols:['レジ台数','占有率','カバレッジ'],
         all :300,
-        rate:80,
         num  :300,
+        getRate:100,
+        cavarege:100,
+        rate:80,
       },
       hzAllData:{
+        title:'②　HZ総拠点数',
+        cols:['ターゲット','拠点数','達成率'],
         all :300,
-        rate:100,
         num  :300,
+        rate:100,
       },
       sdAllData:{
+        title:'③　SD総拠点数',
+        cols:['ターゲット','拠点数','達成率'],
         all :300,
-        rate:86,
         num  :300,
+        rate:86,
       },
       dpData:{
+        title:'④　DP設置台数',
+        cols:['ターゲット','設置台数','達成率'],
         all :300,
-        rate:67,
         num  :300,
+        rate:130,
       },
       displayData:{
+        title:'⑤　大陳列',
+        cols:['ターゲット','大陳回数','達成率'],
         all :300,
-        rate:70,
         num  :300,
+        rate:99,
       },
       inproData:{
+        title:'⑥　インプロ金額',
+        cols:['ターゲット','インプロ金額','達成率'],
         all :300,
-        rate:98,
         num  :300,
+        rate:98,
       },
       startDt:moment(new Date).startOf("month").format('YYYY-MM-DD'),
       endDt:moment(new Date).endOf("month").format('YYYY-MM-DD'),
