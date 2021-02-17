0125768107e79d92efe71a1a92fcfcac102c78fc
neishimasayuki <m-neishi@fiks.co.jp>
2021-02-17 15:45:05 +0900

    共通SQLをClassに含める

--- server/cgi-bin/dbAccessor.py
+++ server/cgi-bin/dbAccessor.py
@@ -20,6 +20,31 @@ class dbAccessor:
         self.cur.execute(sql, bind)
         rows = self.cur.fetchall()
         return rows
-    
+
+    #ターゲット数値取得
+    def getTargetNum(self, qno, month, startdtstr, clientid, year):
+        sql = 'SELECT ktv_chqid, clcq_name as chq, ktv_month%s as `all`, 0 as regi, 0 as num FROM m_kpi_target_value val '\
+                'INNER JOIN t_client_chq ON ktv_chqid = clcq_chqid AND ktv_clientid = clcq_clientid '\
+                'WHERE ktv_delete = 0 AND clcq_delete = 0 '\
+                'AND clcq_term_1 <= %s AND clcq_term_2 >= %s '\
+                'AND ktv_clientid = %s AND ktv_year = %s AND ktv_qno = %s'
+        rows = self.execQuery(sql, [month, startdtstr, startdtstr, clientid, year, qno])
+        #タプル=>辞書型に変換
+        chqs = {}
+        for row in rows:
+            chqs[row['ktv_chqid']] = dict(row)
+        return chqs
+
+    #ターゲット店舗取得
+    def getTargetShop(self, qno, startdtstr, clientid, startYearmonth, endYearmonth):
+        sql = 'SELECT kts_shopid, clsp_chqid FROM m_kpi_target_shop kpi '\
+                'INNER JOIN t_client_shop shop ON kts_shopid = clsp_shopid AND kts_clientid = clsp_clientid '\
+                'WHERE kts_delete = 0 AND clsp_delete = 0 '\
+                'AND clsp_term_1 <= %s AND clsp_term_2 >= %s '\
+                'AND kts_clientid = %s AND kts_yearmonth >= %s AND kts_yearmonth <= %s '\
+                'AND kts_question%s = 1 ORDER BY clsp_chqid ASC'
+        return self.execQuery(sql, [startdtstr, startdtstr, clientid, startYearmonth, endYearmonth, qno])
+
+
     def __del__(self):
         self.conn.close()
--- server/cgi-bin/inputParser.py
+++ server/cgi-bin/inputParser.py
@@ -25,6 +25,9 @@ class inputParser:
         self.enddt = form.getfirst('enddt', '2021-01')
         self.clientid = form.getfirst('clientid', 162)
 
+        self.startdtYearmonth = self.startdt.replace('-','')
+        self.enddtYearmonth = self.enddt.replace('-','')
+
         #開始日終了日の取得
         self.today = datetime.today()
         self.todaystr = datetime.strftime(self.today, '%Y-%m-%d')
